4fbc65a08f44d73f4afac195e720874541c0b95a
NyanSpruk <slimenian@gmail.com>
2022-11-24 08:46:43 +0100

    Completed ProductDTO

--- services/src/main/java/si/fri/prpo/skupina7/dtos/ProductDto.java
+++ services/src/main/java/si/fri/prpo/skupina7/dtos/ProductDto.java
@@ -1,4 +1,26 @@
 package si.fri.prpo.skupina7.dtos;
 
+import si.fri.prpo.skupina7.Category;
+
 public class ProductDto {
+    private Integer productId;
+    private String name;
+    private String description;
+    private Integer price;
+    private Category category;
+
+    public Integr getProductId() {return productId};
+    public void setProductId(Integer productId) { this.productId = productId;}
+
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+
+    public String getDescription() {return description;}
+    public void setDescription(String description) {this.description = description;}
+
+    public Integer getPrice() {return price;}
+    public void setPrice(Integer price) {this.price = price;}
+
+    public Category getCategory() {return category;}
+    public void setCategory(Category category) {this.category = category;}
 }
