9d0269a161b7514964b1281844af412bf4e6bbda
bl4ko <go3969@student.uni-lj.si>
2022-12-01 22:45:19 +0100

    interceptor added

---
+++ services/src/main/java/si/fri/prpo/skupina7/interceptors/NoteCallsInterceptor.java
@@ -0,0 +1,20 @@
+package si.fri.prpo.skupina7.interceptors;
+
+import si.fri.prpo.skupina7.annotations.NoteCalls;
+
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.Interceptor;
+import javax.interceptor.InvocationContext;
+import java.util.logging.Logger;
+
+@Interceptor
+@NoteCalls
+public class NoteCallsInterceptor {
+
+    Logger log = Logger.getLogger(NoteCallsInterceptor.class.getName());
+
+    @AroundInvoke
+    public Object NoteCalls(InvocationContext context) throws Exception {
+        
+    }
+}
