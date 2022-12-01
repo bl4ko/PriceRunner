758c87197e29a745ee472678dae87321b90fe15d
bl4ko <go3969@student.uni-lj.si>
2022-12-01 23:12:23 +0100

    Interceptor sample

--- services/src/main/java/si/fri/prpo/skupina7/interceptors/NoteCallsInterceptor.java
+++ services/src/main/java/si/fri/prpo/skupina7/interceptors/NoteCallsInterceptor.java
@@ -5,7 +5,10 @@ import si.fri.prpo.skupina7.annotations.NoteCalls;
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.Interceptor;
 import javax.interceptor.InvocationContext;
+import java.util.Map;
 import java.util.logging.Logger;
+
+import static java.util.logging.Level.INFO;
 
 @Interceptor
 @NoteCalls
@@ -13,9 +16,20 @@ public class NoteCallsInterceptor {
 
     Logger log = Logger.getLogger(NoteCallsInterceptor.class.getName());
 
+    private Map<String, Integer> method2count;
+
     @AroundInvoke
     public Object NoteCalls(InvocationContext context) throws Exception {
-        /* TODO */
+
+        if (method2count.containsKey(context.getMethod().getName())) {
+            method2count.put(context.getMethod().getName(), method2count.get(context.getMethod().getName()) + 1);
+        } else {
+            method2count.put(context.getMethod().getName(), 1);
+        }
+
+        log.log(INFO, "Method invocated: " + context.getMethod().getName());
+        log.log(INFO, "Number of invocations: " + method2count.get(context.getMethod().getName()));
+
         return null;
     }
 }
