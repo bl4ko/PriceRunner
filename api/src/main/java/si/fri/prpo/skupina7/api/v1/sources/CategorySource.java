7db20921a795ec79470fa3760df079401d6f30dd
NyanSpruk <slimenian@gmail.com>
2022-12-08 10:30:06 +0100

    Add and update category functions

--- api/src/main/java/si/fri/prpo/skupina7/api/v1/sources/CategorySource.java
+++ api/src/main/java/si/fri/prpo/skupina7/api/v1/sources/CategorySource.java
@@ -94,4 +94,44 @@ public class CategorySource {
         categoryBean.deleteCategory(id);
         return Response.ok().build();
     }
+
+    @Operation(description = "Create a new category", summary = "Create a new category")
+    @APIResponses({
+            @APIResponse(
+                    responseCode = "200",
+                    description = "Category created"
+            ),
+            @APIResponse(
+                    responseCode = "400",
+                    description = "Invalid input"
+            )})
+    @POST
+    public Response createCategory(@Parameter(description = "Category to be created", required = true) Category category) {
+        if (category == null) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+        categoryBean.createCategory(category);
+        return Response.ok().build();
+    }
+
+    @Operation(description = "Update a category with given id", summary = "Update a category")
+    @APIResponses({
+            @APIResponse(
+                    responseCode = "200",
+                    description = "Category updated"
+            ),
+            @APIResponse(
+                    responseCode = "404",
+                    description = "Category not found"
+            )})
+    @PUT
+    @Path("{id}")
+    public Response updateCategory(@Parameter(description = "Category ID", required = true) @PathParam("id") Integer id, @Parameter(description = "Category to be updated", required = true) Category category) {
+        Category c = categoryBean.getCategory(id);
+        if (c == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        categoryBean.updateCategory(id, category);
+        return Response.ok().build();
+    }
 }
