80d93665b5cb640f9617419399a21a11ccf90be4
bl4ko <go3969@student.uni-lj.si>
2023-01-01 19:19:16 +0100

    Fixing openapi docs

--- api/src/main/java/si/fri/prpo/skupina7/api/v1/CartApplication.java
+++ api/src/main/java/si/fri/prpo/skupina7/api/v1/CartApplication.java
@@ -1,8 +1,24 @@
 package si.fri.prpo.skupina7.api.v1;
 
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.info.License;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement;
+import org.eclipse.microprofile.openapi.annotations.servers.Server;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/v1")
+@OpenAPIDefinition(info = @Info(
+        title = "PriceRunner API",
+        version = "v1",
+        description = "API for PriceRunner project",
+        license = @License(name = "Apache 2.0")
+), servers = {
+        @Server(url = "http://localhost:8080/"),
+        @Server(url = "https://prpo.bl4ko.com/")
+}, security = @SecurityRequirement(name = "openid-connect")
+)
 public class CartApplication extends Application {
 }
